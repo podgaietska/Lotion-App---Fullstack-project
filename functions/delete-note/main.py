c90ee8b14b7e28f554f1a42017fc0ca30cfa2762
Axeloooo <axelshz@gmail.com>
2023-03-23 14:07:27 -0600

    Delete note lambda function completed

--- functions/delete-note/main.py
+++ functions/delete-note/main.py
@@ -1,1 +1,36 @@
-# add your delete-note function here
+import boto3
+import json
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table("lotion")
+
+# DELETE
+# https://zpmwd5ukdz6qtutfucvwgvdxd40uxdoa.lambda-url.ca-central-1.on.aws/
+
+
+def lambda_handler(event, context):
+    email = event["queryStringParameters"]["email"]
+    id = event["queryStringParameters"]["id"]
+    try:
+        res = table.delete_item(
+            Key={
+                "email": email,
+                "id": id
+            }
+        )
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps({"message": "Item deleted successfully"})
+        }
+    except Exception as e:
+        print(e)
+        return {
+            "statusCode": 500,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps({"error": str(e)})
+        }
