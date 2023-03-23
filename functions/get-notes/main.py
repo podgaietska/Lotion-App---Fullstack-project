f6f4f3697abc881da2b0131355755ed6009d281d
Axeloooo <axelshz@gmail.com>
2023-03-23 14:08:02 -0600

    Get notes lambda function completed

--- functions/get-notes/main.py
+++ functions/get-notes/main.py
@@ -1,1 +1,28 @@
-# add your get-notes function here
+import json
+import boto3
+from boto3.dynamodb.conditions import Key
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table("lotion")
+
+# GET
+# https://had54xrk4jqcdt4qpwsgkfwbp40yeodp.lambda-url.ca-central-1.on.aws/
+
+
+def lambda_handler(event, context):
+    try:
+        email = event["queryStringParameters"]["email"]
+        res = table.query(KeyConditionExpression=Key("email").eq(email))
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps(res["Items"])
+        }
+    except Exception as e:
+        print(e)
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
