72c5ae243c771d9839a7e505f7976b438c7d76bb
Axeloooo <axelshz@gmail.com>
2023-03-23 14:09:03 -0600

    Save note lambda function completed

--- functions/save-note/main.py
+++ functions/save-note/main.py
@@ -1,1 +1,43 @@
-# add your save-note function here
+import json
+import boto3
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table("lotion")
+
+# PUT
+# https://zr5xpblny3rtkvkkls6uhe5vby0vqyoc.lambda-url.ca-central-1.on.aws/
+
+
+def lambda_handler(event, context):
+    email = event["queryStringParameters"]["email"]
+    id = event["queryStringParameters"]["id"]
+    try:
+        data = json.loads(event["body"])
+        res = table.update_item(
+            Key={
+                "email": email,
+                "id": id
+            },
+            UpdateExpression="SET title = :title, content = :content, last_modified = lastModified",
+            ExpressionAttributeValues={
+                ":title": data["title"],
+                ":content": data["content"],
+                ":last_modified": data["lastModified"]
+            }
+        )
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps({"message": "Item updated successfully"})
+        }
+    except Exception as e:
+        print(e)
+        return {
+            "statusCode": 500,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps({"error": str(e)})
+        }
