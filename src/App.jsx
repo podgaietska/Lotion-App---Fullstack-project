016d80320b927882f89ee908b3da7a27e3d31356
Axeloooo <axelshz@gmail.com>
2023-03-24 13:33:58 -0600

    Get, save and delete features implemented and working in AWS

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,7 @@ import axios from "axios";
 
 function App() {
   const [sidebar, setSidebar] = useState(true);
-  const [notes, setNotes] = useState(
-    localStorage.notes ? JSON.parse(localStorage.notes) : []
-  );
+  const [notes, setNotes] = useState([]);
   const [activeNote, setActiveNote] = useState(false);
   const [user, setUser] = useState(null);
   const [profile, setProfile] = useState(null);
@@ -57,10 +55,26 @@ function App() {
   }, [user]);
 
   useEffect(() => {
-    localStorage.setItem("notes", JSON.stringify(notes));
-  }, [notes]);
+    if (profile) {
+      const fetchData = async () => {
+        const res = await fetch(
+          `https://re3kflzgk76iiasr4shxscuaxe0hjqcv.lambda-url.ca-central-1.on.aws/?email=${profile.email}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              "Access-Token": `${user.access_token}`,
+            },
+          }
+        );
+        const data = await res.json();
+        setNotes(data);
+      };
+      fetchData();
+    }
+  }, [user, profile]);
 
-  const onAddNote = () => {
+  const onAddNote = async () => {
     const newNote = {
       id: uuid(),
       title: "Untitled Note",
@@ -72,20 +86,45 @@ function App() {
     setActiveNote(newNote.id);
   };
 
-  const onUpdateNote = (updatedNote) => {
+  const onUpdateNote = async (updatedNote) => {
     const updatedNotesArray = notes.map((note) => {
       if (note.id === activeNote) {
         return updatedNote;
       }
       return note;
     });
+
     setNotes(updatedNotesArray);
+    console.log(user.access_token);
+    console.log(profile.email);
+    await fetch(
+      `https://lqhpngfxpgpasdm74pmsak3dry0aslzn.lambda-url.ca-central-1.on.aws/?email=${profile.email}&id=${updatedNote.id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Token": `${user.access_token}`,
+        },
+        body: JSON.stringify(updatedNote),
+      }
+    );
   };
 
-  const onDeleteNote = (idToDelete) => {
+  const onDeleteNote = async (idToDelete) => {
     const answer = window.confirm("Are you sure?");
     if (answer) {
+      await fetch(
+        `https://m4e54it7rex52xwgf4iojjcwf40zxysj.lambda-url.ca-central-1.on.aws/?email=${profile.email}&id=${idToDelete}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Token": `${user.access_token}`,
+          },
+        }
+      );
       setNotes(notes.filter((note) => note.id !== idToDelete));
+      setActiveNote(false);
     }
   };
 
