656b05d9ea332a76619b0c83b1489307bbdee5b0
Axeloooo <axelshz@gmail.com>
2023-03-24 11:32:32 -0600

    Save note lambda function modified

--- functions/save-note/main.py
+++ functions/save-note/main.py
@@ -4,27 +4,20 @@ import boto3
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table("lotion")
 
-# PUT
-# https://zr5xpblny3rtkvkkls6uhe5vby0vqyoc.lambda-url.ca-central-1.on.aws/
-
 
 def lambda_handler(event, context):
     email = event["queryStringParameters"]["email"]
     id = event["queryStringParameters"]["id"]
     try:
         data = json.loads(event["body"])
-        res = table.update_item(
-            Key={
-                "email": email,
-                "id": id
-            },
-            UpdateExpression="SET title = :title, content = :content, last_modified = lastModified",
-            ExpressionAttributeValues={
-                ":title": data["title"],
-                ":content": data["content"],
-                ":last_modified": data["lastModified"]
-            }
-        )
+        item = {
+            "email": email,
+            "id": id,
+            "title": data["title"],
+            "body": data["body"],
+            "last_modified": data["lastModified"]
+        }
+        res = table.put_item(Item=item)
         return {
             "statusCode": 200,
             "headers": {
