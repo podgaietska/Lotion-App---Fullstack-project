fb7dfb98c3882b7cbb5489a01eb95cd9aec9e5ba
Axeloooo <axelshz@gmail.com>
2023-03-24 11:38:20 -0600

    React app code cleaned

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+// @ts-nocheck
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <GoogleOAuthProvider clientId="172923453068-3n84tl9jdeh3mvgacuh6o2eo28gb9fr2.apps.googleusercontent.com">
     <React.StrictMode>
       <App />
     </React.StrictMode>
   </GoogleOAuthProvider>
-  
 );
-
